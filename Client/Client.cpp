a25a51f1664a05ece3b45c90e198a6682b4c6138
Michael Mandl <mandlm@molez.net>
2015-02-22 20:29:32 +0100

    Testing the serializer

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 #include "proto/client.pb.h"
 
 int main(int argc, char **argv)
 {
 	std::cout << "Client running" << std::endl;
+
+	std::string bufferString;
+
+	client::ClientInfo clientInfoIn;
+
+	clientInfoIn.set_name("Herbert");
+	clientInfoIn.SerializeToString(&bufferString);
+
+	client::ClientInfo clientInfoOut;
+	clientInfoOut.ParseFromString(bufferString);
+
+	std::cout << "read: " << clientInfoOut.name() << std::endl;
 
 	return 0;
 }
